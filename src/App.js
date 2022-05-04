26f2defb5d64a3427c2245e314a8a7e5e038621a
Harsha <harshavadlavalli4@gmail.com>
2022-05-04 08:19:08 +0530

    created login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from "react";
+import './index.css';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
+import LogIn from "./Pages/login/login.jsx"
+import SignUp from "./Pages/signup/signup.jsx"
+import Choose from "./Pages/choose/choose.jsx"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<LogIn/>}></Route>
+        <Route exact path='/signup' element={<SignUp/>}></Route>
+        <Route exact path='/selection' element={<Choose/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
